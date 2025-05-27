80fe0c18fe6a44aeb250b678795c2e1d8a7afdce
Kurusa <51157689+Kurusa@users.noreply.github.com>
2025-05-27 20:42:03 +0300

    Add proper return type to indexFields

--- src/HasExpandableMany.php
+++ src/HasExpandableMany.php
@@ -2,6 +2,7 @@
 
 namespace Lupennat\ExpandableMany;
 
+use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait HasExpandableMany
@@ -9,9 +10,9 @@ trait HasExpandableMany
     /**
      * Resolve the index fields.
      *
-     * @return \Laravel\Nova\Fields\FieldCollection<int, \Laravel\Nova\Fields\Field>
+     * @return FieldCollection<int, \Laravel\Nova\Fields\Field>
      */
-    public function indexFields(NovaRequest $request)
+    public function indexFields(NovaRequest $request): FieldCollection
     {
         return $this->availableFields($request)
             ->when($request->viaManyToMany(), $this->relatedFieldResolverCallback($request))
